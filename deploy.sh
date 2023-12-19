5ed20b92ef8c0a2cb0806bf50e3880d373654e49
joehoct <joehoct@lanecrawford.com>
2023-12-19 09:25:32 +0000

    test

--- deploy.sh
+++ deploy.sh
@@ -199,6 +199,7 @@ if [ "vOPT" = "true" ] || [ "$REPO" = "mirumee" ]; then
                   s|{static}|$STATIC_URL|g
                   s|{media}|$MEDIA_URL|g" $HD/Deploy_Saleor/resources/saleor/server_block | sudo dd status=none of=/etc/nginx/sites-available/saleor
         wait
+        sudo rm -rf /etc/nginx/sites-enabled/saleor
         sudo ln -s /etc/nginx/sites-available/saleor /etc/nginx/sites-enabled/        
 else
         # Create the new service file
@@ -218,6 +219,7 @@ else
                   s|{media}|$MEDIA_URL|g
                   s/{api_port}/$API_PORT/" $HD/Deploy_Saleor/resources/saleor/server_block | sudo dd status=none of=/etc/nginx/sites-available/saleor
         wait
+        sudo rm -rf /etc/nginx/sites-enabled/saleor        
         sudo ln -s /etc/nginx/sites-available/saleor /etc/nginx/sites-enabled/
 fi
 # Create the production uwsgi initialization file
